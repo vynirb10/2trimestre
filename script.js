c7beac9b9ad2354ca48edbd7087bc23141c41c6b
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-08-19 14:44:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        {
+            texto:"Alternativa 1",
+            afirmacao: "Afirmação 1"
+        },
+        {
+        texto:"Alternativa 2",
+        afirmacao: "Afirmação 2"
+        }
+    ]
+},
+
+{
+    enunciado: "Pergunta 2",
+    alternativas: [
+        {
+
+        texto: "Alternativa 1 da pergunta 2",
+        afirmacao: "Afirmação 1 da pergunta 2"
+        },
+        {
+        texto:"Alternativa 1 da pergunta 2",
+        afirmacao: "Afirmação 2 da pergunta 2"
+        }
+    ]
+},
+
+{
+    enunciado: "Pergunta 3",
+    alternativas: [
+        {
+        texto:"Alternativa 1 da pergunta 3",
+        afirmacao: "Afirmação 1 da pergunta 3"
+        },
+        {
+        texto:"Alternativa 2 da pergunta 3",
+        afirmacao: "Afirmação 2 da pergunta 3"
+        }
+    ]
+}
+
+]
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(){
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return; 
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao
+    historiaFinal += afirmacao + " ";
+    atual++;
+    mostraPergunta();
+}
+function mostraResultado(){
+caixaPerguntas.textContent = "Suas ações mudam o mundo!";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
