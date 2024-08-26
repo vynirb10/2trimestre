df07bb0a1ea3d593660391a97849a030b5f6d3bb
Estíverson Oliveira <107802352+EstiversonOliveira@users.noreply.github.com>
2024-08-26 18:53:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,44 +6,44 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-    enunciado: "Pergunta 1",
+    enunciado: "Quais são os desafios enfrentados na criação de tecnologias acessíveis e como podem ser superados?",
     alternativas: [
         {
-            texto:"Alternativa 1",
-            afirmacao: "Afirmação 1"
+            texto:"Custos elevados",
+            afirmacao: "afirmação 1"
         },
         {
-        texto:"Alternativa 2",
-        afirmacao: "Afirmação 2"
+        texto:"Conscientização e educação",
+        afirmacao: "afirmação 2"
         }
     ]
 },
 
 {
-    enunciado: "Pergunta 2",
+    enunciado: "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
     alternativas: [
         {
 
-        texto: "Alternativa 1 da pergunta 2",
-        afirmacao: "Afirmação 1 da pergunta 2"
+        texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
+        afirmacao: "afirmação 3"
         },
         {
-        texto:"Alternativa 2 da pergunta 2",
-        afirmacao: "Afirmação 2 da pergunta 2"
+        texto:"Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
+        afirmacao: "afirmação 4"
         }
     ]
 },
 
 {
-    enunciado: "Pergunta 3",
+    enunciado: "Quais são os benefícios do uso de recursos tecnológicos para promover a acessibilidade em diferentes ambientes?",
     alternativas: [
         {
-        texto:"Alternativa 1 da pergunta 3",
-        afirmacao: "Afirmação 1 da pergunta 3"
+        texto:"Promover a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
+        afirmacao: "afirmação 5"
         },
         {
-        texto:"Alternativa 2 da pergunta 3",
-        afirmacao: "Afirmação 2 da pergunta 3"
+        texto:"Defender políticas públicas que subsidiem a energia solar para famílias de baixa renda.",
+        afirmacao: "afirmação 6"
         }
     ]
 }
